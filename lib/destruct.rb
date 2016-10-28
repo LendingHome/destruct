2ef2a2d15b5f1b00eb78fef5d667cd9d526aa434
Sean Huber <sean@lendinghome.com>
2016-10-27 23:05:23 -0700

    Simplify Destruct module

--- lib/destruct.rb
+++ lib/destruct.rb
@@ -2,22 +2,8 @@ require_relative "destruct/dig"
 require_relative "destruct/hash"
 require_relative "destruct/object"
 
-Object.send(:include, Destruct::Object)
-
 module Destruct
-  GEMSPEC = name.downcase.concat(".gemspec")
-
-  class << self
-    def gemspec
-      @gemspec ||= Gem::Specification.load(root.join(GEMSPEC).to_s)
-    end
-
-    def root
-      @root ||= Pathname.new(__FILE__).dirname.dirname
-    end
-
-    def version
-      gemspec.version.to_s
-    end
-  end
+  ::Object.send(:include, Object)
+  GEMSPEC = File.expand_path("../../destruct.gemspec", __FILE__)
+  VERSION = Gem::Specification.load(GEMSPEC).version.to_s
 end
--- spec/destruct_spec.rb
+++ spec/destruct_spec.rb
@@ -37,22 +37,4 @@ RSpec.describe Destruct do
       expect(status).to eq(200)
     end
   end
-
-	describe "#gemspec" do
-    it "returns a gemspec object" do
-      expect(described_class.gemspec).to be_a(Gem::Specification)
-    end
-  end
-
-  describe "#root" do
-    it "returns a pathname object" do
-      expect(described_class.root).to be_a(Pathname)
-    end
-  end
-
-  describe "#version" do
-    it "returns a version string" do
-      expect(described_class.version).to match(/^\d+\.\d+\.\d+$/)
-    end
-  end
 end
